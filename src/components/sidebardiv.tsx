85d66a82810d4ac6cd13e1836debf457b50c2f69
Simon Beijer <beijersimon@gmail.com>
2020-03-08 11:19:14 +0100

    Functions for api

--- src/components/sidebardiv.tsx
+++ src/components/sidebardiv.tsx
@@ -1,6 +1,8 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, ErrorInfo } from 'react';
 import Items from './items';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { AnyNaptrRecord } from 'dns';
 
 export interface AllItems {
     id: string
@@ -8,22 +10,31 @@ export interface AllItems {
     item: string
     alt: string
 }
-interface Props { 
+interface Props {
     size: string
 }
 
 interface State {
     allItems: AllItems[]
-
+    loaded: boolean
+    items: []
 }
+
 class SidebarDiv extends React.Component<Props, State> {
     private APIKey: string = "hTSax0xjObcGX6GTkOEnKgLLmEALMhW0vuB7We8v";
-    
+    private manifest: string = "manifests";
+    private camera: string = "NAVCAM";
+    private selectedRover: string = "Curiosity";
+    private URL: string = 'https://api.nasa.gov/mars-photos/api/v1/' + this.manifest + "/" + this.selectedRover + "?api_key=" + this.APIKey;
+
+
     constructor(props: Props) {
         super(props)
         this.state = {
+            loaded: false,
+            items: [],
             allItems: [{
-                id: '',
+                id: '1',
                 title: 'mars',
                 item: 'https://images.unsplash.com/photo-1538551868183-edf7bfc50391?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
                 alt: 'space stuff'
@@ -60,9 +71,44 @@ class SidebarDiv extends React.Component<Props, State> {
 
 
 
-    componentDidMount() {
-        console.log("did mount")
+    async componentDidMount() {
+        const response = await fetch(this.URL)
+        const data = await response.json()
+        const filterdList = this.filterManifest(data)
+        this.loadImages(filterdList)
     }
+
+
+    filterManifest(data: any) {
+        let filterdList = data.photo_manifest.photos.filter((photo: any) => {
+            let cameraExists = false;
+            photo.cameras.forEach((camera: string) => {
+                if (camera === "NAVCAM") {
+                    cameraExists = true
+                }
+            });
+            return cameraExists
+        })
+        // console.log(filterdList);
+    }
+
+    async loadImages(filterdList: any) {
+        const response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/' + 'curiosity/photos?sol=1&page=1&camera=' + this.camera + "&api_key=" + this.APIKey)
+        console.log('https://api.nasa.gov/mars-photos/api/v1/rovers' + 'curiosity/photos?sol=1&page=1&camera=' + this.camera + "&api_key=" + this.APIKey);
+        
+        const data = await response.json()
+        console.log(data)
+    }
+
+    
+
+
+
+    // this.setState({
+    //     items: data
+    // })
+
+
 
     render() {
         return (
